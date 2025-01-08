167dcf84fcc7c1d096507e0d8c1c030d4c62a5ad
Adhil Salim <69722917+adhilsalim@users.noreply.github.com>
2025-01-08 04:42:01 +0000

    Fix module import error

---
+++ breezypythongui/__init__.py
@@ -0,0 +1,1 @@
+from .breezypythongui import EasyFrame
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='breezypythongui', 
-    version='1.1.1',
+    name='breezypythongui',
+    version='1.1.2',         
     author='Ken Lambert',
     author_email='lambertk@wlu.edu',
     description='A simple GUI toolkit for Python',
-    packages=['breezypythongui'],  # Include the package directory
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
